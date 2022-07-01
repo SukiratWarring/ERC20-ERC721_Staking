e61e8b1203978cdec37fb93a3e4b150acf1e098a
Sukirat <Sukiratwarring@gmail.com>
2022-07-01 17:59:09 +0530

    Adding_more_tests

--- src/test/Tests.js
+++ src/test/Tests.js
@@ -19,13 +19,13 @@ describe("Token Contract", async () => {
     Nft = await ethers.getContractFactory("staking_erc_to_nft");
     nft = await upgrades.deployProxy(Nft, [erc20_address], { kind: 'uups' });
     await nft.deployed();
-    [owner,add1,add2,...addr]=await ethers.getSigners();
+    [owner, add1, add2, ...addr] = await ethers.getSigners();
     // console.log(owner);
     // console.log(add1);
   });
 
   describe("staking_erc_to_nft", async function () {
-    
+
     it("Should return the name of erc20 and erc721 contract", async function () {
       expect(await erc20.name()).to.equal("Warin");
       expect(await nft.name()).to.equal("WarNFT");
@@ -33,35 +33,52 @@ describe("Token Contract", async () => {
     it("should return the total supply of erc20 contract", async function () {
       expect(await erc20.totalSupply()).to.equal(100000000);
     });
-    it("SHould Transfer tokens from the owner to other accounts",async()=>{
-      await erc20.transfer(add1.address,5);
-      const add1Balance=await erc20.balanceOf(add1.address)
+    it("SHould Transfer tokens from the owner to other accounts", async () => {
+      await erc20.transfer(add1.address, 5);
+      const add1Balance = await erc20.balanceOf(add1.address)
       expect(add1Balance).to.equal(5)
       // console.log(await(erc20.balanceOf(add2.address)))
-      await erc20.connect(add1).transfer(add2.address,5)
-      const add2Balance=await erc20.balanceOf(add2.address);
+      await erc20.connect(add1).transfer(add2.address, 5)
+      const add2Balance = await erc20.balanceOf(add2.address);
       // console.log(add2Balance)
       expect(add2Balance).to.equal(5);
     })
-    it("Check if the Sender has adequate funds",async()=>{
-      const initialownerBalance=await erc20.balanceOf(owner.address);
+    it("Check if the Sender has adequate funds", async () => {
+      const initialownerBalance = await erc20.balanceOf(owner.address);
       console.log(`Thi is initial owner balance ${initialownerBalance}`)
-      const initialadd1Balance=await erc20.balanceOf(add1.address)
+      const initialadd1Balance = await erc20.balanceOf(add1.address)
       console.log(initialadd1Balance);
-      await expect(erc20.connect(add1).transfer(owner.address,1))
-      .to.be.revertedWith("ERC20: transfer amount exceeds balance");
-      expect (await erc20.balanceOf(owner.address)).to.equal(initialownerBalance);
-              
+      await expect(erc20.connect(add1).transfer(owner.address, 1))
+        .to.be.revertedWith("ERC20: transfer amount exceeds balance");
+      expect(await erc20.balanceOf(owner.address)).to.equal(initialownerBalance);
+
     });
-    it("Should update balances after transfer",async()=>{
-      const initialownerBalance=await erc20.balanceOf(owner.address);
-      await erc20.transfer(add1.address,20);
-      await erc20.transfer(add2.address,30);
-      expect(await erc20.balanceOf(owner.address)).to.equal(initialownerBalance-50)
-      
+    it("Should update balances after transfer", async () => {
+      const initialownerBalance = await erc20.balanceOf(owner.address);
+      await erc20.transfer(add1.address, 20);
+      await erc20.transfer(add2.address, 30);
+      expect(await erc20.balanceOf(owner.address)).to.equal(initialownerBalance - 50)
+
     })
     //Writing Tests for NFT CONTRACT
-    
-    
+    it("Checking the mapping",async()=>{
+      await nft.create_stake_and_mint_nft(owner.address,2,10,20000);
+      console.log(`This is the mapping output ${await nft.addToStake(owner.address)}`);
+      const [output2]=await nft.addToStake(owner.address);
+      expect(output2).to.equal(20000)
+    })
+    it("Check if the person has staked anything", async () => {
+      await expect(nft.reward_per_person(add1.address)).to.be.revertedWith("User has not staked any thing yet");
+    })
+    it("Check if the reward is calculated correctly",async()=>{
+      await nft.create_stake_and_mint_nft(owner.address,10,10,20000);
+      await nft.reward_per_person(owner.address);
+      const reward= await nft.addTOreward(owner.address);
+      console.log(reward)
+      expect(reward).to.equal(800);
+      
+    })
+
+
   })
 });
